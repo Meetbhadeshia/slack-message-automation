835674e6a2fc5263c3890b1c7dc1ab3ad18af983
Ashish <ashish@hyperzepinfotech.com>
2023-07-26 18:16:49 +0530

    new deployment

--- index.py
+++ index.py
@@ -38,8 +38,7 @@ def print_hello_world():
 def wait_until_morning():
     now = datetime.now()
     tomorrow_morning = now.replace(
-        hour=18, minute=10, second=0, microsecond=0)
-    # + timedelta(days=1)
+        hour=10, minute=0, second=0, microsecond=0) + timedelta(days=1)
     time_to_wait = (tomorrow_morning - now).total_seconds()
     print(time_to_wait)
     time.sleep(time_to_wait)
